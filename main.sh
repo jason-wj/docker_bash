271a84194314e674804269b8bb480b8da8d00e02
idea_wj@163.com <a9h8b4a9h8b4>
2022-03-14 13:51:29 +0800

    update file

--- main.sh
+++ main.sh
@@ -50,7 +50,7 @@ COMMAND_ETCD2="etcd2"
 COMMAND_ETCD3="etcd3"
 COMMAND_ETCDUI="etcdui"
 COMMAND_ARBNODE="arbnode"
-COMMAND_DTM="dtm"
+COMMAND_DTF="dtf"
 
 # 容器版本
 export IMAGE_MONGO="mongo"
@@ -68,7 +68,7 @@ export IMAGE_CONSUL="consul"
 export IMAGE_ETCD="bitnami/etcd:latest"
 export IMAGE_ETCDUI="evildecay/etcdkeeper"
 export IMAGE_ARBNODE="offchainlabs/arb-node:v1.1.2-cffb3a0"
-export IMAGE_DTM="yedf/dtm:latest"
+export IMAGE_DTF="yedf/dtm:latest"
 
 # container：必须保留，当一个容器涉及到多个依赖时，方便选择加入
 export CONTAINER_MONGO=${PROJECT_NAME}"-"${COMMAND_MONGO}
@@ -91,7 +91,7 @@ export CONTAINER_ETCD2=${PROJECT_NAME}"-"${COMMAND_ETCD2}
 export CONTAINER_ETCD3=${PROJECT_NAME}"-"${COMMAND_ETCD3}
 export CONTAINER_ETCDUI=${PROJECT_NAME}"-"${COMMAND_ETCDUI}
 export CONTAINER_ARBNODE=${PROJECT_NAME}"-"${COMMAND_ARBNODE}
-export CONTAINER_DTM=${PROJECT_NAME}"-"${COMMAND_DTM}
+export CONTAINER_DTF=${PROJECT_NAME}"-"${COMMAND_DTF}
 
 # 根据不同项目模式切换镜像，同时
 if [[ ${PROJECT_MODE} == "prod" ]]; then
